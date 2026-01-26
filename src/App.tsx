779e40d9b562bd7030ea0d4eba7c0330021f5562
Victor Souza <victor.souza@somoshbi.com.br>
2026-01-26 18:04:18 -0300

    feat: adicionar estilo para a classe 'vermelho' e atualizar a estrutura do componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import Contador from "./components/Contador";
+import './App.css';
 
 function App() {
-  const [numero, setNumero] = useState(0);
-
-  function aumentar() {
-    setNumero(numero + 1);
-  }
 
   return (
 
-    <div>
-      <h1>Total Geral: {numero}</h1>
-      {/* Quero que este comece do zero */}
-      <Contador valorInicial={numero} acao={aumentar} />
+    <div className='principal'>
+      <div className="vermelho">
+        <h1>Vermelho</h1>
+      </div>
+      <div>
+        <h1>Azul</h1>
+      </div>
+      <div>
+        <h1>Verde</h1>
+      </div>
 
     </div>
   )
